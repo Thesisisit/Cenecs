0a459d40b7742dfad38d79bb122696acf0e78739
michaelignalaga@gmail.com <>
2019-12-03 15:51:30 +0800

    mehehe

--- codes.py
+++ codes.py
@@ -42,29 +42,30 @@ class Window(Frame):
         swidth = self.winfo_screenwidth()
         sheight = self.winfo_screenheight()
 
-
-        load = Image.open("C:\\Users\\Electric-Post.png")
+        load = Image.open("Electric-Post.png")
         load = load.resize((swidth, sheight), Image.ANTIALIAS)
         render = ImageTk.PhotoImage(load)
         img = Label(self, image=render)
         img.image = render
         img.place(x=0, y=0)
 
-
-
-
         can = Canvas(self, height=100, width=20)
         can.place(relx=0.5, rely=0.5, anchor=CENTER)
         entry = Entry(can, font=("Calibri 100"), justify="center")
         entry.pack(ipady=3)
         entry.focus()
+    def add_window(self,):
+        window = Toplevel(root)
+        Frame.deiconify
+if __name__ == "__main__":
+    root = Tk()
+    app = Window(root)
+    root.wm_title("CENECO PAYMENT MACHINE")
+    root.geometry("1920x1080")
+    button = Button(root,  text="Press ENTER to Confirm", bg="Green", fg="Black",width=20, command= Window.add_window, font = ("TimesNewRoman 24"), justify="center")
+    button.place(x=750, y=650)
+    #root.bind('<Return>', create_window)
+    root.bind('<0>', final)
+    root.mainloop()
 
-root = Tk()
-app = Window(root)
-root.wm_title("CENECO PAYMENT MACHINE")
-root.geometry("1920x1080")
-button = Button(root,  text="Press ENTER to Confirm", bg="Green", fg="Black",width=20, command=lambda: create_window(None), font = ("TimesNewRoman 24"), justify="center")
-button.place(x=750, y=650)
-root.bind('<Return>', create_window)
-root.bind('<0>', final)
-root.mainloop()
+
